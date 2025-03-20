f05ce44d2e1cfb64561558099b0323ab3b0c5ee0
12yasaswi56 <yasaswikopparapu624@gmail.com>
2025-03-20 22:24:36 +0530

    fc

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ app.use(express.json());
 app.use(cors());
 
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads"), {
+  setHeaders: (res) => {
+      res.set("Cross-Origin-Resource-Policy", "cross-origin");
+  },
+}));
+
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
